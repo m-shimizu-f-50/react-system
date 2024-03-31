420fc22662b5c2fc12b3d3854f0417ccd2780979
simizu-masato <m.shimizu@trigger4.com>
2024-03-31 15:34:05 +0900

    Material UI ハンバーガーメニュー/レスポンシブ

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,11 @@ import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import Logo from '../assets/images/logo512.png';
 
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from 'react';
+import Drawer from '@mui/material/Drawer';
+
 const setNavLinks: Array<{ text: string; url: string }> = [
   { text: 'Top', url: '/' },
   { text: 'Profile', url: '/profile' },
@@ -19,15 +24,24 @@ const setNavLinks: Array<{ text: string; url: string }> = [
 ];
 
 const Header: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <AppBar component='header' position='static'>
         <Container maxWidth='md'>
           <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Box>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
               <Typography component='h1'>
                 <Link to='/'>
-                  <img src={Logo} alt='画像' height='60' width='auto' />
+                  <img src={Logo} alt='画像' height='50' width='auto' />
                 </Link>
               </Typography>
             </Box>
@@ -36,10 +50,24 @@ const Header: React.FC = () => {
                 component='nav'
                 sx={{ display: 'flex', justifyContent: 'flex-start' }}
               >
+                <ListItem disablePadding>
+                  <ListItemButton
+                    sx={{
+                      textAlign: 'center',
+                      display: { xs: 'block', md: 'none' },
+                    }}
+                    onClick={handleDrawerOpen}
+                  >
+                    <ListItemText primary={<MenuIcon />} />
+                  </ListItemButton>
+                </ListItem>
                 {setNavLinks.map((nav) => (
                   <ListItem disablePadding key={nav.text}>
                     <ListItemButton
-                      sx={{ textAlign: 'center' }}
+                      sx={{
+                        textAlign: 'center',
+                        display: { xs: 'none', md: 'block' },
+                      }}
                       component={Link}
                       to={nav.url}
                     >
@@ -50,6 +78,50 @@ const Header: React.FC = () => {
               </List>
             </Box>
           </Box>
+          <Drawer
+            anchor='right'
+            open={open}
+            onClose={handleDrawerClose}
+            PaperProps={{
+              style: {
+                width: '100%',
+                backgroundColor: '#1976d2',
+                color: '#fff',
+              },
+            }}
+          >
+            <List
+              component='nav'
+              sx={{ display: 'block', justifyContent: 'normal' }}
+            >
+              <ListItem disablePadding>
+                <ListItemButton
+                  sx={{
+                    textAlign: 'center',
+                    borderBottom: 'solid 1px #696969',
+                  }}
+                  onClick={handleDrawerClose}
+                >
+                  <ListItemText primary={<CloseIcon />} />
+                </ListItemButton>
+              </ListItem>
+              {setNavLinks.map((nav) => (
+                <ListItem disablePadding key={nav.text}>
+                  <ListItemButton
+                    sx={{
+                      textAlign: 'center',
+                      borderBottom: 'solid 1px #696969',
+                    }}
+                    onClick={handleDrawerClose}
+                    component={Link}
+                    to={nav.url}
+                  >
+                    <ListItemText primary={nav.text} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
         </Container>
       </AppBar>
     </>
